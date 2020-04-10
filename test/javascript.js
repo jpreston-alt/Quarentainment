ac9cb118636dfada19c78ade14293710a10729d1
Jared Hennessy <jared.hennessy@gmail.com>
2020-04-09 21:53:47 -0700

    adding test subdirectory

---
+++ test/javascript.js
@@ -0,0 +1,7 @@
+var tmdbApiKey = "53fb3173ef10002db2f58ad55ccf032e";
+var tmdbQueryUrl =
+  "https://api.themoviedb.org/3/movie/550?api_key=" + tmdbApiKey;
+
+var nytApiKey = "GOOGHDHZGwdBBruE3XTXgj3TIcGoewXU";
+var nytBooksUrl = "https://api.nytimes.com/svc/books/v3/";
+var nytMovies = "https://api.nytimes.com/svc/movies/v2/";
