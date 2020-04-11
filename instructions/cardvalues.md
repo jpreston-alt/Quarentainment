49e89bc022bc9d77f5de7461836bdeba8eb82d2f
Jared Hennessy <jared.hennessy@gmail.com>
2020-04-11 13:59:37 -0700

    nyt results looking alright

---
+++ instructions/cardvalues.md
@@ -0,0 +1,22 @@
+# BOOKS
+
+1. title / author
+1. image
+1. link (purchase)
+1. summary
+
+# MOVIES
+
+1. title / rating
+1. image
+1. link (review) AND/OR score
+1. genre
+1. summary
+
+# TV
+
+1. title / rating
+1. image
+1. score
+1. genre
+1. summary
--- nyt/movies.js
+++ nyt/movies.js
@@ -8,21 +8,50 @@ $(document).ready(function () {
     url: nytMovieListUrl,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
     for (i = 0; i < response.results.length; i++) {
       var divMovie = $("<p>").html(
         response.results[i].display_title +
           "<br />" +
           response.results[i].mpaa_rating +
-          "<br /><img src = '" +
+          "<br /><img src= '" +
           response.results[i].multimedia.src +
           "'><br/><a href='" +
           response.results[i].link.url +
-          "'>Read the Review</a><br/>" +
+          "' target='_blank'>Read the Review</a><br/>" +
           response.results[i].summary_short +
           "<br/>"
       );
       $("#movieList").append(divMovie);
     }
   });
+
+  function getReviewLink(title) {
+    nytMovieSearchUrl =
+      nytMoviesUrl +
+      "/reviews/search.json?query=" +
+      title +
+      "&api-key=" +
+      nytApiKey;
+    $.ajax({
+      url: nytMovieSearchUrl,
+      method: "GET",
+      // success: function (responseJSON) {
+      //   console.log(responseJSON);
+      //   return responseJSON;
+      // },
+    }).done(function (response) {
+      var reviewLink = response.results[0].link.url;
+      console.log(reviewLink);
+      console.log(response);
+      return response;
+    });
+  }
+
+  console.log(getReviewLink("the lion king"));
+  // console.log(getReviewLink("the lion king").responseJSON);
+  // console.log(getReviewLink("the lion king").responseText);
+
+  // var link = getReviewLink("the lion king").responseJSON.results[0].link.url;
+  // console.log(link);
+  // $("#searchResult").attr("href", link);
 });
