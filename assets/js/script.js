889b2413adbd4aab1a8aa184c29912075fc2e7dd
Joanna Preston <joannappreston@gmail.com>
2020-04-11 12:37:51 -0700

    Added ability to render browse page based on the type of media chosen. Saves media type to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+var mediaType;
+getStorage();
+renderBrowsePage();
+
 
 $(document).ready(function () {
 
@@ -15,12 +19,110 @@ $(document).ready(function () {
             $(this).toggleClass("is-active");
             $navbarMenuID.toggleClass("is-active");
         })
-    })
+    });
 
-    $(".nav-to-books").on("click", function(event) {
-        event.preventDefault();
-        console.log("clicked books")
-        $("#media-type").text("Books");
-    })
+    // call event handlers for when user clicks on movies, books, or shows links
+    clickMediaType("movies");
+    clickMediaType("books");
+    clickMediaType("shows");
+
+
+
+    var nytApiKey = "GOOGHDHZGwdBBruE3XTXgj3TIcGoewXU";
+    var nytBooksUrl = "https://api.nytimes.com/svc/books/v3";
+    var nytBookListsUrl = nytBooksUrl + "/lists/names.json?api-key=" + nytApiKey;
+
+    $.ajax({
+        url: nytBookListsUrl,
+        method: "GET",
+    }).then(function (response) {
+        for (i = 0; i < response.results.length; i++) {
+            var listYear = parseInt(
+                response.results[i].newest_published_date.substring(0, 4)
+            );
+            if (listYear >= 2019) {
+                var bookGenre = response.results[i].display_name;
+                console.log(bookGenre);
+
+
+                // var listName = response.results[i].display_name;
+                // var listItem = $("<li>").text(listName);
+                // $("#bookLists").append(listItem);
+            }
+        }
+    });
 
 });
+
+// sets local storage
+function setStorage() {
+
+    // media type
+    localStorage.setItem("mediaType", mediaType);
+};
+
+
+// pulls from local storage
+function getStorage() {
+
+    // saves media type to variable
+    mediaType = localStorage.getItem("mediaType");
+};
+
+
+// renders browse page depending on media type variable
+function renderBrowsePage() {
+    renderDropdown(mediaType);
+
+    var mediaTypeEl = $("#media-type")
+
+    if (mediaType === "movies") {
+        mediaTypeEl.text("Trending Movies of the Week")
+    } else if (mediaType === "shows") {
+        mediaTypeEl.text("Trending TV Shows of the Week")
+    } else if (mediaType === "books") {
+        mediaTypeEl.text("Trending Books of the Week")
+    }
+};
+
+// function for event handler when user clicks on media link
+function clickMediaType(type) {
+    $('.nav-to-' + type ).on("click", function (event) {
+
+        // only if you're own the browse page preventDefault
+        if ($("body").is(".browse-page")) {
+            event.preventDefault();
+        };
+
+        // empty dropdown options so they can be refilled
+        $("#media-dropdown").empty();
+        mediaType = type;
+        setStorage();
+        renderBrowsePage();
+    });
+};
+
+// function to render drop down based on media type
+function renderDropdown(type) {
+    var dropdownEl = $("#media-dropdown")
+    var movieGenres = ["Action", "Horror", "Drama", "Comedy"];
+    var tvGenres = ["Sitcom"];
+    var bookGenres = ["Fiction", "Non-Fiction", "Science"];
+
+    if (type === "movies") {
+        arr = movieGenres;
+    } else if (type === "shows") {
+        arr = tvGenres;
+    } else {
+        arr = bookGenres;
+    };
+
+    for (var i = 0; i < arr.length; i++) {
+        var $newOption = $("<option>")
+        $newOption.text(arr[i]);
+        dropdownEl.append($newOption);
+    };
+
+};
+
+
