65a7779a55b2e5c939d920ce57aa260397a7ef32
Joanna Preston <joannappreston@gmail.com>
2020-04-14 10:03:34 -0700

    Renders top trending tv shows to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,77 @@ var mediaType;
 var genreList = [];
 var cardsArr = [];
 var listSelection;
+var mediaTypeEl = $("#media-type");
 
-function MediaCard(title, author, imgURL, link, summary) {
+function MediaCard(title, authorOrRating, imgURL, linkOrGenre, summary) {
     this.title = title;
-    this.author = author;
+    this.authorOrRating = authorOrRating;
     this.imgURL = imgURL;
-    this.link = link;
+    this.linkOrGenre = linkOrGenre;
     this.summary = summary;
 };
 
 var title;
-var author;
+var authorOrRating;
 var imgURL;
-var link;
+var linkOrGenre;
 var summary;
 
+// movie genres
+var genreDictionMovies = {
+    28: "Action",
+    12: "Adventure",
+    16: "Animation",
+    35: "Comedy",
+    80: "Crime",
+    99: "Documentary",
+    18: "Drama",
+    10751: "Family",
+    14: "Fantasy",
+    36: "History",
+    27: "Horror",
+    10402: "Music",
+    10749: "Romance",
+    878: "Science Fiction",
+    10770: "TV Movie",
+    53: "Thriller",
+    10752: "War",
+    37: "Western",
+};
 
+// tv show genres
+var genreDictionTV = {
+    10759: "Action & Adventure",
+    16: "Animation",
+    35: "Comedy",
+    80: "Crime",
+    99: "Documentary",
+    18: "Drama",
+    10751: "Family",
+    10762: "Kids",
+    9648: "Mystery",
+    10763: "News",
+    10764: "Reality",
+    10765: "Sci-Fi & Fantasy",
+    10766: "Soap",
+    10767: "Talk",
+    10768: "War & Politics",
+    37: "Western",
+    28: "Action",
+    12: "Adventure",
+    14: "Fantasy",
+    36: "History",
+    27: "Horror",
+    10402: "Music",
+    10749: "Romance",
+    878: "Science Fiction",
+    10770: "TV Movie",
+    53: "Thriller",
+    10752: "War",
+};
+
+
+// event listeners
 $(document).ready(function () {
 
     // event listener for hamburger drop down menu
@@ -34,7 +89,7 @@ $(document).ready(function () {
         })
     });
 
-    // call event handlers for when user clicks on movies, books, or shows links
+    // call event handlers for when user clicks on movies, books, or shows linkOrGenres
     clickMediaType("movies");
     clickMediaType("books");
     clickMediaType("shows");
@@ -42,13 +97,20 @@ $(document).ready(function () {
 
     // event handler for when user changes genre on dropdown menu and clicks search button
     $("#dropdown-search-btn").on("click", function() {
-       var genreSelection = ($("#dropdown-form").find("#media-dropdown").val()).replace(/\s+/g, '-');
-       console.log(genreSelection);
+       var genreSelection = ($("#dropdown-form").find("#media-dropdown").val());
 
-        listSelection = genreSelection;
+        listSelection = genreSelection.replace(/\s+/g, '-');
         setStorage();
-        renderBrowsePage();
-    })
+
+        if (mediaType === "books") {
+            mediaTypeEl.text("Top Selling " + genreSelection);
+            changeBookCards();
+        } else if (mediaType === "movies") {
+            changeMovieCards();
+        } else if (mediaType === "shows") {
+            changeTvShowCards();
+        }
+    });
 
 });
 
@@ -72,13 +134,9 @@ function getStorage() {
     };
 };
 
-// function for event handler when user clicks on media link
+// function for event handler when user clicks on media linkOrGenre
 function clickMediaType(type) {
-    $('.nav-to-' + type).on("click", function (event) {
-        if (type === "books") {
-            listSelection = "hardcover-fiction";
-        };
-
+    $('.nav-to-' + type).on("click", function () {
         mediaType = type;
         setStorage();
         renderBrowsePage();
@@ -102,27 +160,24 @@ function renderTrendingCards() {
     // create new card elements based on how many objects are in the cardsArray
     for (var i = 0; i < cardsArr.length; i++) {
 
-        var mediaCardEl = $('<div class="column is-half"><div class= "card"><div class="card-content columns is-mobile"><div class="column"><img src="' + cardsArr[i].imgURL + '" class="media-img"></div><div class="column"><p class="title media-title">' + cardsArr[i].title + '</p><p class="subtitle media-author">' + cardsArr[i].author + '</p><a class="subtitle media-link" href="' + cardsArr[i].link + '">Purchase Here</a><p class="subtitle media-summary">' + cardsArr[i].summary + '</p></div></div><footer class="card-footer"><a href="#" class="card-footer-item add-to-list-btn">Add to My List</a></footer></div></div >')
+        var mediaCardEl = $('<div class="column is-half"><div class= "card media-card"><div class="card-content columns is-mobile"><div class="column"><img src="' + cardsArr[i].imgURL + '" class="media-img"></div><div class="column is-scrollable"><p class="title media-title">' + cardsArr[i].title + '</p><p class="subtitle media-authorOrRating">' + cardsArr[i].authorOrRating + '</p><p>' + cardsArr[i].linkOrGenre + '</p><br><p class="subtitle media-summary">' + cardsArr[i].summary + '</p></div></div><footer class="card-footer"><a href="#" class="card-footer-item add-to-list-btn">Add to My List</a></footer></div></div >')
 
         // append new card element to content container
         $("#browse-content-container").append(mediaCardEl);
-
     };
-
 };
 
 
-// renders browse page depending on media type variable- pulls data from appropriate API
+// renders trending browse page depending on media type variable
 function renderBrowsePage() {
 
     // empty cards container and cardsArr
     $("#browse-content-container").empty();
     cardsArr = [];
 
-    var mediaTypeEl = $("#media-type");
-
     if (mediaType === "books") {
-        mediaTypeEl.text("Trending Books of the Week")
+        listSelection = "hardcover-fiction";
+        mediaTypeEl.text("Top Selling Books of the Week")
 
         var nytApiKey = "GOOGHDHZGwdBBruE3XTXgj3TIcGoewXU";
         var nytBooksUrl = "https://api.nytimes.com/svc/books/v3";
@@ -146,75 +201,23 @@ function renderBrowsePage() {
             renderDropdown();
         });
 
-        
-
-        var nytBookTitlesUrl =
-            nytBooksUrl + "/lists/current/" + listSelection + "?api-key=" + nytApiKey;
-
-        $.ajax({
-            url: nytBookTitlesUrl,
-            method: "GET",
-        }).then(function (bookResponse) {
-            for (j = 0; j < bookResponse.results.books.length; j++) {
-
-                // save data to variables
-                title = bookResponse.results.books[j].title;
-                author = bookResponse.results.books[j].contributor;
-                imgURL = bookResponse.results.books[j].book_image;
-                link = bookResponse.results.books[j].amazon_product_url;
-                summary = bookResponse.results.books[j].description;
-
-                // create new MediaCard object with variables
-                var card = new MediaCard(title, author, imgURL, link, summary);
-
-                // push new MediaCard to cardsArr
-                cardsArr.push(card);
-            }
-
-            // render Trending cards to screen
-            renderTrendingCards();
-        });
-
+        changeBookCards();
 
     } else if (mediaType === "movies") {
 
         // change page title
         mediaTypeEl.text("Trending Movies of the Week")
 
-        var trendURL = "https://api.themoviedb.org/3/trending/movie/day?api_key=660bf8330423e5658590b1cdb677dc08"
-        // var apiKey = "660bf8330423e5658590b1cdb677dc08";
-
-        var genreDiction = {
-            28: "Action",
-            12: "Adventure",
-            16: "Animation",
-            35: "Comedy",
-            80: "Crime",
-            99: "Documentary",
-            18: "Drama",
-            10751: "Family",
-            14: "Fantasy",
-            36: "History",
-            27: "Horror",
-            10402: "Music",
-            10749: "Romance",
-            878: "Science Fiction",
-            10770: "TV Movie",
-            53: "Thriller",
-            10752: "War",
-            37: "Western",
-        };
-
-        // render dropdown menu using genreDiction values
-        var genresArr = Object.values(genreDiction);
-        
+        // render dropdown based on genres
+        var genresArr = Object.values(genreDictionMovies);
         for (var i = 0; i < genresArr.length; i++) {
             genreList.push(genresArr[i]);
         };
-
         renderDropdown();
 
-        // call trending movies data
+        // get trending movies data
+        var trendURL = "https://api.themoviedb.org/3/trending/movie/day?api_key=660bf8330423e5658590b1cdb677dc08"
+
         $.ajax({
             url: trendURL,
             method: "GET"
@@ -227,21 +230,21 @@ function renderBrowsePage() {
                 var myGenreids = response.results[i].genre_ids;
                 var anyString = ""
                 for (var j = 0; j < myGenreids.length; j++) {
-                    anyString = anyString + ", " + genreDiction[myGenreids[j]]
+                    anyString = anyString + ", " + genreDictionMovies[myGenreids[j]]
                 };
 
                 var resString = anyString.substring(2);
 
                 // save movie data to variables
                 title = response.results[i].title;
-                imgURL = "https://image.tmdb.org/t/p/w200/" + response.results[i].poster_path;
-                // response.results[i].vote_average
-                // resString
+                imgURL = "https://image.tmdb.org/t/p/w300/" + response.results[i].poster_path;
+                authorOrRating =  response.results[i].vote_average
+                linkOrGenre = resString;
                 summary = response.results[i].overview;
 
 
                 // create new MediaCard object with variables
-                var card = new MediaCard(title, author, imgURL, link, summary);
+                var card = new MediaCard(title, authorOrRating, imgURL, linkOrGenre, summary);
 
                 // push new MediaCard to cardsArr
                 cardsArr.push(card);
@@ -257,12 +260,104 @@ function renderBrowsePage() {
         // call tv show data
         mediaTypeEl.text("Trending TV Shows of the Week")
 
+        // render dropdown based on genres
+        var genresArr = Object.values(genreDictionTV);
+        for (var i = 0; i < genresArr.length; i++) {
+            genreList.push(genresArr[i]);
+        };
+        renderDropdown();
+
+        // get trending TV data
+        var tQuery = "https://api.themoviedb.org/3/trending/tv/day?api_key=660bf8330423e5658590b1cdb677dc08"
+
+        $.ajax({
+            url: tQuery,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+
+            for (var i = 0; i < response.results.length; i++) {
+
+                var myGenreids = response.results[i].genre_ids;
+                var anyString = ""
+                for (var j = 0; j < myGenreids.length; j++) {
+                    anyString = anyString + ", " + genreDictionTV[myGenreids[j]]
+                }
+
+                var resString = anyString.substring(2);
+
+                
+                title = response.results[i].original_name;
+                imgURL = "https://image.tmdb.org/t/p/w300/" + response.results[i].poster_path;
+                authorOrRating= response.results[i].vote_average + " out of 10";
+                linkOrGenre = resString
+                summary = response.results[i].overview;
+
+                // create new MediaCard object with variables
+                var card = new MediaCard(title, authorOrRating, imgURL, linkOrGenre, summary);
+
+                // push new MediaCard to cardsArr
+                cardsArr.push(card);
+
+            };
+
+            renderTrendingCards();
+        });
+
     }
 };
 
-function changeDropdown() {
+// change book cards when genre is switched from dropdown menu
+function changeBookCards() {
 
+    $("#browse-content-container").empty();
+    cardsArr = [];
+
+    var nytApiKey = "GOOGHDHZGwdBBruE3XTXgj3TIcGoewXU";
+    var nytBooksUrl = "https://api.nytimes.com/svc/books/v3";
+    var nytBookTitlesUrl =
+        nytBooksUrl + "/lists/current/" + listSelection + "?api-key=" + nytApiKey;
+
+    $.ajax({
+        url: nytBookTitlesUrl,
+        method: "GET",
+    }).then(function (bookResponse) {
+        for (j = 0; j < bookResponse.results.books.length; j++) {
+
+            // save data to variables
+            title = bookResponse.results.books[j].title;
+            authorOrRating = bookResponse.results.books[j].contributor;
+            imgURL = bookResponse.results.books[j].book_image;
+            linkOrGenre = '<a class="subtitle media-linkOrGenre" href="' + bookResponse.results.books[j].amazon_product_url + '">Purchase Here</a>'
+            summary = bookResponse.results.books[j].description;
+
+            // create new MediaCard object with variables
+            var card = new MediaCard(title, authorOrRating, imgURL, linkOrGenre, summary);
+
+            // push new MediaCard to cardsArr
+            cardsArr.push(card);
+        }
+
+        // render Trending cards to screen
+        renderTrendingCards();
+    });
 };
+
+// change movie cards when genre is switched from dropdown menu
+function changeMovieCards() {
+    console.log("changed movie genre");
+};
+
+
+// change tv show cards when genre is switched from dropdown menu
+function changeTvShowCards() {
+    console.log("changed tv show genre");
+};
+
+
+
+
+
 
 // define init function
 function init() {
